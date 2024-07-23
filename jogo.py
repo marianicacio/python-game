f348d6fc1312d4408135081c31ab02aaaf37560e
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2024-07-23 14:32:28 -0300

    feat: python game

--- jogo.py
+++ jogo.py
@@ -6,7 +6,7 @@ pygame.init()#para abrir o pygame
 window = pygame.display.set_mode([1280,720])
 #aplicar o título da janela/display
 pygame.display.set_caption("Futebol SENAI")
-#criar variáveis para as imagens:
+#criar variáveis para as img:
 campo = pygame.image.load("img/campo.png")
 jogador1 = pygame.image.load("img/player1.png")
 jogador1_y = 310
@@ -23,11 +23,11 @@ score2_img = pygame.image.load("img/0.png")
 #colocar a bolinha para rolar
 bola_x = 617
 bola_y = 337
-bola_dir = -5
+bola_dir = -15
 bola_dir_y = 1
 
 def draw():
-    #Carregar as imagens
+    #Carregar as img
     window.blit(campo,(0,0))
     window.blit(jogador1,(50, jogador1_y))
     window.blit(jogador2,(1150,jogador2_y))
@@ -35,26 +35,30 @@ def draw():
     window.blit(score1_img,(500,50))
     window.blit(score2_img,(710,50))
 
-
 def move_bola():
     global bola_x
     global bola_y
     global bola_dir
     global bola_dir_y
+    global score1
+    global score2
+    global score1_img
+    global score2_img
+
     #Para mover a bola para a direita
     bola_x += bola_dir
     bola_y += bola_dir_y
-
+    
     if bola_x < 123:
         if jogador1_y < bola_y + 23:
-            if jogador1_y + 146> bola_y:
+            if jogador1_y + 146 > bola_y:
                 bola_dir *= -1
-
+    
     if bola_x > 1100:
         if jogador2_y < bola_y + 23:
-            if jogador2_y + 146> bola_y:
+            if jogador2_y + 146 > bola_y:
                 bola_dir *= -1
-
+    
     if bola_y > 685:
         bola_dir_y *= -1
     elif bola_y <= 0:
@@ -65,35 +69,40 @@ def move_bola():
         bola_y = 337
         bola_dir *= -1
         bola_dir_y *= -1
+        score2 += 1
+        score2_img = pygame.image.load('img/' + str(score2) + ".png")
     elif bola_x > 1320:
         bola_x = 617
         bola_y = 337
         bola_dir *= -1
         bola_dir_y *= -1
+        score1 += 1
+        score1_img = pygame.image.load('img/' + str(score1) + ".png")
+
 
 
 def move_jogador():
-    global jogador1_y
+    global jogador1_y  
     if jogador1_y_moveup:
-        jogador1_y-=5
+        jogador1_y -= 5
     else:
-        jogador1_y_moveup
-        jogador1_y+=0
-    if jogador1_y_movedown:
-        jogador1_y+=5
-    else:
-        jogador1_y_movedown
-        jogador1_y+=0
+        jogador1_y += 0
 
-    if jogador1_y<=0:
+    if jogador1_y_movedown:
+        jogador1_y += 5
+    else:
+        jogador1_y += 0
+
+    if jogador1_y <= 0:
         jogador1_y = 0
     elif jogador1_y >= 575:
         jogador1_y = 575
 
 def move_jogador2():
     global jogador2_y
-    jogador2_y = bola_y
-    
+    jogador2_y = bola_y      
+
+
 #para manter a janela aberta:
 loop = True
 while loop:
@@ -111,6 +120,8 @@ while loop:
                 jogador1_y_moveup = False
             if event.key == pygame.K_s:
                 jogador1_y_movedown = False
+            
+            
     draw()
     move_bola()
     move_jogador()
